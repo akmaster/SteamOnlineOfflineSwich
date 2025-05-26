d3de802f4f9475023e5dd6a083438c79a30ca06d
akmaster <akmaster1991@gmail.com>
2025-05-26 19:30:31 +0300

    Remove outdated pull request and issue templates, along with associated release files, to streamline the repository structure and eliminate unnecessary clutter.

--- release/GameClientController.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-title Game Client Connection Controller
-echo Starting Game Client Connection Controller...
-echo.
-echo Note: This program requires administrator privileges.
-echo.
-GameClientController.exe
-pause
